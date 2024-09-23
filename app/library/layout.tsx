bec3df3d15b479722607a9245687587bcce24255
Sion Wilks-Boguszewicz <58896718+sioncamara@users.noreply.github.com>
2024-09-23 16:42:15 -0500

    fix: hide inimplemented search and profile (#12)

--- app/library/layout.tsx
+++ app/library/layout.tsx
@@ -19,15 +19,15 @@ export default function LibraryLayout({
   return (
     <div className="flex flex-auto flex-col pb-[16px]">
       <div className="flex gap-4 self-end pb-[60px] pt-[16px]">
-        <div className="relative ml-auto flex-1 md:grow-0">
+        {/* <div className="relative ml-auto flex-1 md:grow-0">
           <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-slate-500 dark:text-slate-400" />
           <Input
             className="w-full rounded-lg bg-white pl-8 md:w-[200px] lg:w-[336px] dark:bg-slate-950 dark:text-slate-50"
             placeholder="Search..."
             type="search"
           />
-        </div>
-        <DropdownMenu>
+        </div> */}
+        {/* <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button
               className="overflow-hidden rounded-full"
@@ -55,7 +55,7 @@ export default function LibraryLayout({
             <DropdownMenuSeparator />
             <DropdownMenuItem>Logout</DropdownMenuItem>
           </DropdownMenuContent>
-        </DropdownMenu>
+        </DropdownMenu> */}
       </div>
 
       {children}
